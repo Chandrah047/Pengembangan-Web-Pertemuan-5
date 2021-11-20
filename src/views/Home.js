9d3d3b65c97477693a57246f63c3d655002cc931
Chandrah047 <192111997@students.mikroskil.ac.id>
2021-11-20 12:16:35 +0700

    Update Home.js

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,5 +1,25 @@
 import React from 'react';
 
+//Latihan 5.5, State
+export default class Home extends React.Component {
+	
+	constructor(props){
+		super(props);
+		this.state = {name : this.props.name}
+	}
+		
+	render() {
+		return (
+			<div>
+				<h2>Title Home Component</h2>
+				<p>Hello my name is {this.props.name}</p>
+				<button onclick={()=> this.setState({name : 'Tyrion Lannister'
+				}) }> Change my name </button>
+			</div>
+		)
+	}
+}
+
 //Latihan 5.4
 export default class Home extends React.Component{ 
 	render() {
