1d30b7c1a96f93b7f1f711c283e6627f74067f2b
Chandrah047 <192111997@students.mikroskil.ac.id>
2021-11-20 11:47:10 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,78 +1,24 @@
 import React, { Component } from 'react';
-import './css/bootstrap.min.css';
 import './App.css';
-import Home from './views/Home/Home';
 
-//Latihan 4.4, Props
-function Message(props) {
-	return <p>Hello {props.name},</p>;
+//Latihan 4.1, Penggunana function pada Reactjs
+function sayHello() {
+	return "Hello World";
 }
 
 class App extends Component {
+	bilangHalo() {
+		return "Halo Dunia";
+	}
 	
 	render() {
 		return (
-			<div>
-				<Message name="Ulrich" />
-				<Home name="Eden Hazard" />
+		<div>
+				<p>sayHello : <b>{sayHello()}</b></p>
+				<p>bilangHalo : <b>{this.bilangHalo()}</b>
 			</div>
-		);
+		)
 	}
 }
 
 export default App;
-
-//Latihan 4.3, Class Component
-//function Message() { 
-//	return "Pesan didalam Component";
-//}
-
-//class App extends Component {
-	
-//	render() {
-//		return (
-//			<div>
-//				<Message />
-//				<Home />
-//			<div>
-//		}
-//	}
-//}
-
-
-
-//Latihan 4.2, penggunaan function javascript sebagai komponen
-//function Message() {
-//	return <p>Hello from message component</p>
-//}
-
-//class App extends Component {
-//	render() {
-//		return (
-//			<div>
-//				<Message />
-//			<div>
-//		}
-//	}
-//}
-
-//Latihan 4.1, Penggunana function pada Reactjs
-//function sayHello() {
-//	return "Hello World";
-//}
-
-//class App extends Component {
-//	bilangHalo() {
-//		return "Halo Dunia";
-//	}
-	
-//	render() {
-//		return {
-//			<>
-//				<p>sayHello : <b>{sayHello()}</b></p>
-//				<p>bilangHalo : <b>{this.bilangHalo()}</b>
-//			</>
-//		}
-//	}
-//}
-	
