659678d2d57c8d610d56de269ecab283eb3d66f4
Chandrah047 <192111997@students.mikroskil.ac.id>
2021-11-20 12:23:25 +0700

    Update Input.js

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Input.css';
 
+//Latihan 5.10, props function
 export default class Input extends Component {
 	constructor(){
 		super();
@@ -23,15 +24,22 @@ export default class Input extends Component {
 	}
 }
 	
-//Latihan 4.9, Update state dengan input
-//	render(){
-//		return (
-//			<div className="input">
-//			
-//				<input type="text" name="status" placeholder="Enter Your Status"
-//				onChange={(ev)=> this.setState({status : ev.target.value})} />
-//				<p>Your status : {this.state.status}</p>
-//			</div>
-// 		);
+
+//Latihan 5.9, Update state dengan input
+// export default class Input extends Component {
+// 	constructor(){
+// 		super();
+		
+// 		this.state = {status : ''}
 // 	}
-// }
+// 	render(){
+// 		return (
+// 			<div className="input">
+			
+// 				<input type="text" name="status" placeholder="Enter Your Status"
+// 				onChange={(ev)=> this.setState({status : ev.target.value})} />
+// 				<p>Your status : {this.state.status}</p>
+// 			</div>
+//  		);
+//  	}
+//  }
