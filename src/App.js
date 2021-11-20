a9807382ad953e23b7382b6b86070c520de441d9
Chandrah047 <192111997@students.mikroskil.ac.id>
2021-11-20 11:19:58 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,81 +2,25 @@ import React, { Component } from 'react';
 import './css/bootstrap.min.css';
 import './App.css';
 import Home from './views/Home/Home';
-import Home from './views/About/About';
-import Home from './views/Help/Help';
 
+//Latihan 4.4, Props
+function Message(props) {
+	return <p>Hello {props.name},</p>;
+}
 
 class App extends Component {
 	
-constructor(){
-	super();
-	this.state = {
-		view : 'home'
+	render() {
+		return (
+			<div>
+				<Message name="Ulrich" />
+				<Home name="Eden Hazard" />
+			</div>
+		);
 	}
 }
 
-	render(){
-		const View = ()=>{
-		if(this.state.view == 'home')
-		return <Home name="Eden Hazard" />
-		else if(this.state.view == 'about')
-		return <About />
-		else if(this.state.view == 'help')
-		return <Help />
-	}
-
-	return (
-		<div> 
-			<nav className="navbar navbar-expand-sm bg-dark navbar-dark">
-				<ul className="navbar-nav">
-					<li className="nav-item">
-						<a onClick={()=> this.setState({view : 'home'})}
-						className="nav-link" href="#">
-							Home
-						</a>
-					</li>
-
-					<li className="nav-item">
-						<a onClick={()=> this.setState({view : 'about'})}
-						className="nav-link" href="#">
-							About
-						</a>
-					</li>
-
-					<li className="nav-item">
-						<a onClick={()=> this.setState({view : 'help'})}
-						className="nav-link" href="#">
-							Help
-						</a>
-					</li>
-				</ul>
-			</nav>
-
-			<View />
-		</div>
-	);
-	}
-}
-
-
-//Latihan 4.4, Props
-//function Message(props) {
-//	return <p>Hello {props.name},</p>;
-//}
-
-//class App extends Component {
-	
-//	render() {
-//		return (
-//			<div>
-//				<Message name="Ulrich" />
-//				<Home name="Eden Hazard" />
-//			<div>
-//		);
-//	}
-//}
-
-
+export default App;
 
 //Latihan 4.3, Class Component
 //function Message() { 
@@ -131,6 +75,4 @@ constructor(){
 //		}
 //	}
 //}
-
-export default App;
 	
