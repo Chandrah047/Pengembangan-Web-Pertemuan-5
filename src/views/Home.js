bb0ebb85647200cc910023f2d5ce351019ee8aa7
Chandrah047 <192111997@students.mikroskil.ac.id>
2021-11-20 12:12:32 +0700

    Update Home.js

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,15 +1,27 @@
 import React from 'react';
 
-//Latihan 5.3
-export default class Home extends React.Component{ 	
+//Latihan 5.4
+export default class Home extends React.Component{ 
 	render() {
 		return (
 		<div>
 			<h2> Title Home Component </h2>
-			<p> Content Home component </p>
+			<p> Hello my name is {this.props.name} </p>
 		</div>
 		)
 	}
 }
+
+//Latihan 5.3
+// export default class Home extends React.Component{ 	
+// 	render() {
+// 		return (
+// 		<div>
+// 			<h2> Title Home Component </h2>
+// 			<p> Content Home component </p>
+// 		</div>
+// 		)
+// 	}
+// }
 
 export default App;
